005d8941bf0b0e7d13fd370c6b4791829a75264e
DimasNuryadin <dimasnuryadinn@gmail.com>
2025-08-22 13:53:10 +0700

    done tugas frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,93 @@
-import Image from "next/image";
+"use client"
+import { SiswaProps } from "@/services/data-types";
+import { useEffect, useState } from "react"
+
+const urlAPI = 'http://localhost:3001';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [dataSiswa, setDataSiswa] = useState([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat("en-CA", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+    }).format(date)
+  }
+
+  useEffect(() => {
+    fetchDataSiswa();
+  }, [])
+
+  async function fetchDataSiswa() {
+    const data = await fetch(`${urlAPI}`);
+    const res = await data.json()
+    setDataSiswa(res.data);
+  }
+
+  async function handleDelete(id: number) {
+    await fetch(`${urlAPI}/${id}`, { method: "DELETE" })
+    fetchDataSiswa();
+  }
+
+  return (
+    <div className="text-center px-30 py-10">
+      <h1 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Data Siswa</h1>
+      <div className="relative overflow-x-auto mt-10">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Kode
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Nama
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Alamat
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Tanggal
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Jurusan
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Aksi
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              dataSiswa.map((siswa: SiswaProps) => {
+                return (
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200" key={siswa.id}>
+                    <td className="px-6 py-4">
+                      {siswa.kode}
+                    </td>
+                    <td className="px-6 py-4">
+                      {siswa.nama}
+                    </td>
+                    <td className="px-6 py-4">
+                      {siswa.alamat}
+                    </td>
+                    <td className="px-6 py-4">
+                      {formatDate(siswa.tanggal)}
+                    </td>
+                    <td className="px-6 py-4">
+                      {siswa.jurusan}
+                    </td>
+                    <td className="px-6 py-4">
+                      <button onClick={() => handleDelete(siswa.id)} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 cursor-pointer">Delete</button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ services/data-types.ts
@@ -0,0 +1,8 @@
+export interface SiswaProps {
+  id: number,
+  kode: string,
+  nama: string,
+  alamat: string,
+  tanggal: string,
+  jurusan: string
+}
